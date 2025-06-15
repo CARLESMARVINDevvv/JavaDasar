d2a9fea14667dd7534a7d9dc531ba0a86ccb2b75
CARLES MARVIN <charlesmarvin4321@gmail.com>
2025-06-15 10:16:34 +0700

    update materi java ifstatement

--- ifStatement.java
+++ ifStatement.java
@@ -3,13 +3,15 @@
 // hampir di smua bahasa pemrograman ada konsep if ini
 public class ifStatement {
     public static void main(String[] args) {
-    
-        var nilaiAkhir = 90;
+        var nilaiAkhir = 55;
+
 
         if (nilaiAkhir >= 75) {
             System.out.println("Selamat Anda Lulus");
         } else if (nilaiAkhir >= 60) {
-            System.out.println("Anda Harus Remedial");
+            System.out.println("Anda Harus Remedial"); {}
+        } else if (nilaiAkhir >= 50) {
+            System.out.println("Anda Harus Mengulang");
         } else {
             System.out.println("Maaf Anda Tidak Lulus");
         }
