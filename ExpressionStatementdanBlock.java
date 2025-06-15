d14f5cad6dd355d91674dc7b4d422a3af82470fa
CARLES MARVIN <charlesmarvin4321@gmail.com>
2025-06-15 09:29:26 +0700

    update materi java

--- ExpressionStatementdanBlock.java
+++ ExpressionStatementdanBlock.java
@@ -1,9 +1,22 @@
-
+import java.util.Date;
 public class ExpressionStatementdanBlock {
     public static void main(String[] args) {
+        // expression 
         int a = 10;
         int  Value = 100;
+        System.out.println(Value = 200);
+        
 
-        System.out.println(Value = 200); 
+
+
+        // Statement
+        // assignment statement
+        double aValue = 8933.234;
+        // increment statement
+        aValue++;
+        // method invocation statement
+        System.out.println("Hello World");
+        // object creation statement
+        Date date = new Date();
     }
 }
