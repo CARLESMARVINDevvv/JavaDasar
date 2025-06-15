5253cf29146d4830b5bb6063e601c902da72678d
CARLES MARVIN <charlesmarvin4321@gmail.com>
2025-06-15 10:09:13 +0700

    update materi java

---
+++ ifStatement.java
@@ -0,0 +1,17 @@
+// dalam java if adalah salah satu kunci yg digunakan untuk membuat percabangan
+// percabangan artinya kita bisa mengksekusi kode progam tertentu ketika suatu kondisi terpenuhi
+// hampir di smua bahasa pemrograman ada konsep if ini
+public class ifStatement {
+    public static void main(String[] args) {
+    
+        var nilaiAkhir = 90;
+
+        if (nilaiAkhir >= 75) {
+            System.out.println("Selamat Anda Lulus");
+        } else if (nilaiAkhir >= 60) {
+            System.out.println("Anda Harus Remedial");
+        } else {
+            System.out.println("Maaf Anda Tidak Lulus");
+        }
+    }
+}
